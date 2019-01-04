86414e0b28077f1d8db909cb1ed7505b9d0cf561
aaronblanco <aaronba13@hotmail.com>
2019-01-04 21:16:44 +0100

    test

--- log_funcion.php
+++ log_funcion.php
@@ -2,7 +2,7 @@
 
 function write_log($cadena,$tipo)
 {
- $arch = fopen(realpath( '.' )."logs/log_".date("Y-m-d").".txt", "a+");
+ $arch = fopen(realpath( '.' )."/logs/log_".date("Y-m-d").".txt", "a+");
 
  fwrite($arch, "[".date("Y-m-d H:i:s.u")." ".$_SERVER['REMOTE_ADDR']." ".
                   $_SERVER['HTTP_X_FORWARDED_FOR']." - $tipo ] ".$cadena."\n");
