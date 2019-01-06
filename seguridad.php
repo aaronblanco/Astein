82d455d00eb9393d237d52f717fce6f0e9de4e60
aaronblanco <aaronba13@hotmail.com>
2019-01-06 12:17:34 +0100

    more changes.

--- seguridad.php
+++ seguridad.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['name']))
-	header("location:admin_login.php");
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+} else {
+    echo "Please log in first to see this page.";
+}
 ?>
