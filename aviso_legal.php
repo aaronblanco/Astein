7a8bad7d7b7264949cfbf1b38a621e50a710ec3f
Niklas Kunz <nk081@hdm-stuttgart.de>
2019-01-15 14:26:25 +0100

    new aviso legal from testing

--- aviso_legal.php
+++ aviso_legal.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
+
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link rel="stylesheet" href="css/EstilosGenerales.css">
+  <link rel="stylesheet" href="css\inicio.css">
+  <link rel="stylesheet" href="css\EstilosGenerales.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="icon" href="images\astein_icon.png"/>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>Aviso legal</title>
 </head>
 <body>
 
@@ -15,80 +16,80 @@
     include "usuario_navbar.php";
   ?>
 
-<div id="main-content">
+  <title> Aviso Legal </title>
+</head>
 
-<div class="aviso-legal">
+<body>
 
-<h1>Aviso legal</h1>
-
-<p>
-  <h3>DATOS GENERALES</h3>
+  <div id="main-content">
+  <h1>Datos generales</h1>
+  <p align= "centered">
   De acuerdo con el artículo 10 de la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y de Comercio Electrónico ponemos a su disposición los siguientes datos:
   Astein está domiciliada en la Carretera de Almeria 86 Oficina 5, con CIF (número correspondiente), correo.numero. Inscrita en el Registro Mercantil de Huercal de Almeria, Vol. (número correspondiente), Folio (número correspondiente), Hoja (número correspondiente), Inscripción (número correspondiente).  //PEDiR REGISTRO MERCANTIL
-  <br><br>
+  <br/>
   En la web www.astein.net  hay una serie de contenidos de carácter informativo sobre las actividades que realizamos, intermediamos entre el cliente y la empresa que facilita la oferta publicada, buscando satisfacer y facilitar la contratación de estas ofertas a nuestros clientes.
-  <br><br>
-  Su principal objetivo es facilitar a los clientes y al público en general, la información relativa a la empresa, a los productos y servicios que se ofrecen (esto se puede modificar según la actividad a la que se dedique la empresa).
-  <br><br>
+</p>
 
+  <h1>Política de privacidad </h1>
 
-  <h3>POLÍTICA DE PRIVACIDAD</h3>
-  En cumplimiento de lo dispuesto en la Ley Orgánica 15/1999, de 13 de Diciembre, de Protección de Datos de Carácter Personal (LOPD) se informa al usuario que todos los datos que nos proporcione serán incorporados a un fichero, creado y mantenido bajo la responsabilidad de Astein.
-  <br><br>
-  Siempre se va a respetar la confidencialidad de sus datos personales que sólo serán utilizados con la finalidad de gestionar los servicios ofrecidos, atender a las solicitudes que nos plantee, realizar tareas administrativas, así como remitir información técnica, comercial o publicitaria por vía ordinaria o electrónica.
-  <br><br>
-  Para ejercer sus derechos de oposición, rectificación o cancelación deberá dirigirse a la sede de la empresa Carretera de Almeria 86 Oficina 5, escribirnos al siguiente correo dptoinformatico@astein.net o llámanos al 643563593.
-  <br><br>
+    <p align= "centered">
+      En cumplimiento de lo dispuesto en la Ley Orgánica 15/1999, de 13 de Diciembre, de Protección de Datos de Carácter Personal (LOPD) se informa al usuario que todos los datos que nos proporcione serán incorporados a un fichero, creado y mantenido bajo la responsabilidad de Astein.
+<br/>
+Siempre se va a respetar la confidencialidad de sus datos personales que sólo serán utilizados con la finalidad de gestionar los servicios ofrecidos, atender a las solicitudes que nos plantee, realizar tareas administrativas, así como remitir información técnica, comercial o publicitaria por vía ordinaria o electrónica.
+<br/>
+Para ejercer sus derechos de oposición, rectificación o cancelación deberá dirigirse a la sede de la empresa Carretera de Almeria 86 Oficina 5, escribirnos al siguiente correo dptoinformatico@astein.net o llámanos al 643563593.
 
-  <h3>CONDICIONES DE USO</h3>
-  Las condiciones de acceso y uso del presente sitio web se rigen por la legalidad vigente y por el principio de buena fe comprometiéndose el usuario a realizar un buen uso de la web. No se permiten conductas que vayan contra la ley, los derechos o intereses de terceros.
-  <br><br>
-  Ser usuario de la web de www.astein.net implica que reconoce haber leído y aceptado las presentes condiciones y lo que las extienda la normativa legal aplicable en esta materia. Si por el motivo que fuere no está de acuerdo con estas condiciones no continúe usando esta web.
-  Cualquier tipo de notificación y/o reclamación solamente será válida por notificación escrita y/o correo certificado.
-  <br><br>
+</P>
 
-  <h3>RESPONSABILIDADES</h3>
-  Astein no se hace responsable de la información y contenidos almacenados en foros, redes sociales o cualesquier otro medio que permita a terceros publicar contenidos de forma independiente en la página web del prestador.
-  <br><br>
-  Sin embargo, teniendo en cuenta los art. 11 y 16 de la LSSI-CE, Astein se compromete a la retirada o en su caso bloqueo de aquellos contenidos que pudieran afectar o contravenir la legislación nacional o internacional, derechos de terceros o la moral y el orden público.
-  <br><br>
-  Tampoco la empresa se responsabilizará de los daños y perjuicios que se produzcan por fallos o malas configuraciones del software instalado en el ordenador del internauta.Se excluye toda responsabilidad por alguna incidencia técnica o fallo que se produzca cuando el usuario se conecte a internet. Igualmente no se garantiza la inexistencia de interrupciones o errores en el acceso al sitio web.
-  <br><br>
-  Así mismo, Astein se reserva el derecho a actualizar, modificar o eliminar la información contenida en su página web, así como la configuración o presentación del mismo, en cualquier momento sin asumir alguna responsabilidad por ello.
-  <br><br>
-  Le comunicamos que cualquier precio que pueda ver en nuestra web será solamente orientativo. Si el usuario desea saber con exactitud el precio o si el producto en el momento actual cuenta con alguna oferta de la cual se puede beneficiar ha de llamar o acudir a nuestra oficina.
-  <br><br>
+<h1>Condiciones de Uso </h1>
+<p align= "centered">
+Las condiciones de acceso y uso del presente sitio web se rigen por la legalidad vigente y por el principio de buena fe comprometiéndose el usuario a realizar un buen uso de la web. No se permiten conductas que vayan contra la ley, los derechos o intereses de terceros.
+<br/>
+Ser usuario de la web de www.astein.net implica que reconoce haber leído y aceptado las presentes condiciones y lo que las extienda la normativa legal aplicable en esta materia. Si por el motivo que fuere no está de acuerdo con estas condiciones no continúe usando esta web.
+Cualquier tipo de notificación y/o reclamación solamente será válida por notificación escrita y/o correo certificado.
+</p>
 
-  <h3>PROPIEDAD INTELECTUAL E INDUSTRIAL</h3>
-  Astein es titular de todos los derechos sobre el software de la publicación digital así como de los derechos de propiedad industrial e intelectual referidos a los contenidos que se incluyan, a excepción de los derechos sobre productos y servicios de carácter público que no son propiedad de esta empresa.
-  <br><br>
-  Ningún material publicado en esta web podrá ser reproducido, copiado o publicado sin el consentimiento por escrito de Astein.  //No se si quitarlo
-  <br><br>
-  Toda la información que se reciba en la web, como comentarios, sugerencias o ideas, se considerará cedida a Astein de manera gratuita. No debe enviarse información que NO pueda ser tratada de este modo.
-  <br><br>
-  Todos los productos y servicios de estas páginas que NO son propiedad de Astein son marcas registradas de sus respectivos propietarios y son reconocidas como tales por nuestra empresa. Solamente aparecen en la web de Astein a efectos de promoción y de recopilación de información. Estos propietarios pueden solicitar la modificación o eliminación de la información que les pertenece.
-  <br><br>
+<h1>Responsabilidades </h1>
+<p align= "centered">
 
-  <h3>LEY APLICABLE Y JURISDICCIÓN</h3>
-  Las presentes condiciones generales se rigen por la legislación española. Para cualquier litigio que pudiera surgir relacionado con el sitio web o la actividad que en él se desarrolla serán competentes Juzgados de Almería, renunciando expresamente el usuario a cualquier otro fuero que pudiera corresponderle.
-  <br><br>
-
-  <h3>POLÍTICA DE COOKIES</h3>
-  Astein por su propia cuenta o la de un tercero contratado para prestación de servicios de medición, pueden utilizar cookies cuando el usuario navega por el sitio web. Las cookies son ficheros enviados al navegador por medio de un servicio web con la finalidad de registrar las actividades del usuario durante su tiempo de navegación.
-  <br><br>
-  Las cookies utilizadas se asocian únicamente con un usuario anónimo y su ordenador, y no proporcionan por sí mismas los datos personales del usuario.
-  Mediante el uso de las cookies resulta posible que el servidor donde se encuentra la web reconozca el navegador web utilizado por el usuario con la finalidad de que la navegación sea más sencilla. Se utilizan también para medir la audiencia y parámetros del tráfico, controlar el proceso y número de entradas.
-  <br><br>
-  El usuario tiene la posibilidad de configurar su navegador para ser avisado de la recepción de cookies y para impedir su instalación en su equipo. Por favor consulte las instrucciones y manuales de su navegador para ampliar esta información.
-  Para utilizar el sitio web no es necesario que el usuario permita la instalación de las cookies enviadas al sitio web, o el tercero que actúe en su nombre, sin perjuicio de que sea necesario que el usuario inicie una sesión tal en cada uno de los servicios cuya prestación requiera el previo registro.
-  <br><br>
-  En todo caso las cookies tienen un carácter temporal con la única finalidad de hacer más eficaz su transmisión ulterior. En ningún caso se utilizará cookies para recoger información de carácter personal.
+Astein no se hace responsable de la información y contenidos almacenados en foros, redes sociales o cualesquier otro medio que permita a terceros publicar contenidos de forma independiente en la página web del prestador.
+<br/>
+Sin embargo, teniendo en cuenta los art. 11 y 16 de la LSSI-CE, Astein se compromete a la retirada o en su caso bloqueo de aquellos contenidos que pudieran afectar o contravenir la legislación nacional o internacional, derechos de terceros o la moral y el orden público.
+<br/>
+Tampoco la empresa se responsabilizará de los daños y perjuicios que se produzcan por fallos o malas configuraciones del software instalado en el ordenador del internauta.Se excluye toda responsabilidad por alguna incidencia técnica o fallo que se produzca cuando el usuario se conecte a internet. Igualmente no se garantiza la inexistencia de interrupciones o errores en el acceso al sitio web.
+<br/>
+Así mismo, Astein se reserva el derecho a actualizar, modificar o eliminar la información contenida en su página web, así como la configuración o presentación del mismo, en cualquier momento sin asumir alguna responsabilidad por ello.
+<br/>
+Le comunicamos que cualquier precio que pueda ver en nuestra web será solamente orientativo. Si el usuario desea saber con exactitud el precio o si el producto en el momento actual cuenta con alguna oferta de la cual se puede beneficiar ha de llamar o acudir a nuestra oficina.
 
 </p>
 
-</div>
+<h1>Propiedad Intelectual e Industrial</h1>
+<p align= "centered">
+Astein es titular de todos los derechos sobre el software de la publicación digital así como de los derechos de propiedad industrial e intelectual referidos a los contenidos que se incluyan, a excepción de los derechos sobre productos y servicios de carácter público que no son propiedad de esta empresa.
+<br/>
+Ningún material publicado en esta web podrá ser reproducido, copiado o publicado sin el consentimiento por escrito de Astein.  //No se si quitarlo
+<br/>
+Toda la información que se reciba en la web, como comentarios, sugerencias o ideas, se considerará cedida a Astein de manera gratuita. No debe enviarse información que NO pueda ser tratada de este modo.
+<br/>
+Todos los productos y servicios de estas páginas que NO son propiedad de Astein son marcas registradas de sus respectivos propietarios y son reconocidas como tales por nuestra empresa. Solamente aparecen en la web de Astein a efectos de promoción y de recopilación de información. Estos propietarios pueden solicitar la modificación o eliminación de la información que les pertenece.
 
-</div>
+</p>
+
+<h1>Politica de Cookies</h1>
+<p align= "centered">
+  Astein por su propia cuenta o la de un tercero contratado para prestación de servicios de medición, pueden utilizar cookies cuando el usuario navega por el sitio web. Las cookies son ficheros enviados al navegador por medio de un servicio web con la finalidad de registrar las actividades del usuario durante su tiempo de navegación.
+<br/>
+Las cookies utilizadas se asocian únicamente con un usuario anónimo y su ordenador, y no proporcionan por sí mismas los datos personales del usuario.
+Mediante el uso de las cookies resulta posible que el servidor donde se encuentra la web reconozca el navegador web utilizado por el usuario con la finalidad de que la navegación sea más sencilla. Se utilizan también para medir la audiencia y parámetros del tráfico, controlar el proceso y número de entradas.
+<br/>
+El usuario tiene la posibilidad de configurar su navegador para ser avisado de la recepción de cookies y para impedir su instalación en su equipo. Por favor consulte las instrucciones y manuales de su navegador para ampliar esta información.
+Para utilizar el sitio web no es necesario que el usuario permita la instalación de las cookies enviadas al sitio web, o el tercero que actúe en su nombre, sin perjuicio de que sea necesario que el usuario inicie una sesión tal en cada uno de los servicios cuya prestación requiera el previo registro.
+<br/>
+En todo caso las cookies tienen un carácter temporal con la única finalidad de hacer más eficaz su transmisión ulterior. En ningún caso se utilizará cookies para recoger información de carácter personal.
+
+</p>
+
 
 <?php
   include "usuario_footer.php";
